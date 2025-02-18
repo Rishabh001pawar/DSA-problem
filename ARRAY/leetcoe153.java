9c6c59000fe637a6df471c2c576dff0df7c31c3b
Rishabh Farkade <rishabhpawar059@gmail.com>
2025-02-18 13:31:53 +0530

    hello

---
+++ ARRAY/leetcoe153.java
@@ -0,0 +1,23 @@
+package ARRAY;
+
+public class leetcoe153 {
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        int low=0; 
+        int high=n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>nums[high]){
+                low=mid+1;
+            }else {
+                high=mid;
+            }
+        }
+        return nums[low];
+    }
+    public static void main(String[] args) {
+        int []nums={3,4,5,1,2};
+        leetcoe153 sol=new leetcoe153();
+        System.out.println(sol.findMin(nums));
+    }
+}
